cbfa14aaa2881f9aa20aacb50b4c893f1bef2674
BaccanoB <beatrice.baccano@yahoo.it>
2021-07-12 16:51:48 +0200

    modificato titolo della pagina film

--- resources/views/film-list.blade.php
+++ resources/views/film-list.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista Libri</title>
+    <title>Lista Film</title>
 </head>
 <body>
     <h1>Ecco qui la lista dei film:</h1>
